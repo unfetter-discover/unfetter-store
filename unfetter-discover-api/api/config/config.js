5468a0d8e06986fcd356aa1e99cd09042b9038b9
j987987 <jason@purplelabs.io>
2017-11-13 09:37:46 -0500

    added run mode check on config file read

--- unfetter-discover-api/api/config/config.js
+++ unfetter-discover-api/api/config/config.js
@@ -1,14 +1,18 @@
-const fs = require('fs');
-const path = require('path');
+if (process.env.RUN_MODE === 'UAC') {
+    const fs = require('fs');
+    const path = require('path');
 
-const privateConfigLocation = path.join(__dirname, 'private-config.json');
-let privateConfig;
+    const privateConfigLocation = path.join(__dirname, 'private-config.json');
+    let privateConfig;
 
-if (!fs.existsSync(privateConfigLocation)) {
-    console.error('Please use unfetter-store/unfetter-utils/api_configuration_tool.py to create private-config.json');
-    process.exit(1);
-} else { 
-    privateConfig = JSON.parse(fs.readFileSync(privateConfigLocation, 'utf8'));
+    if (!fs.existsSync(privateConfigLocation)) {
+        console.error('Please use unfetter-store/unfetter-utils/api_configuration_tool.py to create private-config.json');
+        process.exit(1);
+    } else {
+        privateConfig = JSON.parse(fs.readFileSync(privateConfigLocation, 'utf8'));
+    }
+
+    module.exports = privateConfig;
+} else {
+    module.exports = {};
 }
-
-module.exports = privateConfig;
