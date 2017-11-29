f9b75e36e19937a2a359d3166484163e9c53bd11
mushinlogit <mushinlogit@gmail.com>
2017-11-29 16:10:22 -0500

    fix sample script

--- unfetter-ctf-ingest/test-scripts/translate_url.sh
+++ unfetter-ctf-ingest/test-scripts/translate_url.sh
@@ -1,4 +1,4 @@
 # !/bin/sh
 
 # npm start
-curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json'  -d '{"type":"123","url":"http://localhost/123"}' http://localhost:10010/translate/report/url
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json'  -d '{"systemName":"123","url":"http://localhost/123"}' http://localhost:10010/translate/report/url
