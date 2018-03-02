f05c86c0c4831ba235be2c9cea33bb623a75431f
mushinlogit <mushinlogit@gmail.com>
2018-03-02 13:04:00 -0500

    load values on startup, better lint

--- unfetter-discover-api/api/server/mongoinit.js
+++ unfetter-discover-api/api/server/mongoinit.js
@@ -2,14 +2,44 @@ process.env.MONGO_REPOSITORY = process.env.MONGO_REPOSITORY || 'localhost';
 process.env.MONGO_PORT = process.env.MONGO_PORT || 27018;
 process.env.MONGO_DBNAME = process.env.MONGO_DBNAME || 'stix';
 
+const modelFactory = require('../controllers/shared/modelFactory');
 const mongoose = require('mongoose');
+
+const identityModel = modelFactory.getModel('identity');
+
+const mongoDebug = process.env.MONGO_DEBUG || true;
+mongoose.set('debug', mongoDebug);
 mongoose.Promise = global.Promise;
 
-module.exports = () => {
-    if (global.conn === undefined) {
+/**
+ * @description populate global lookup values
+ */
+const lookupGlobalValues = () => {
+    console.log('looking up global values...');
+    if (global.unfetter.identities === undefined) {
+        identityModel
+            .findOne({ 'stix.type': 'identity', 'stix.name': 'Unfetter Open' })
+            .exec((err, result) => {
+                if (err) {
+                    return;
+                }
+
+                const openIdent = { ...result };
+                global.unfetter.identities = [
+                    ...(global.unfetter.identities || []),
+                    openIdent,
+                ];
+                global.unfetter.openIdentity = openIdent;
+                console.log('set open identity with id, ', openIdent._id || '');
+            });
+    }
+};
+module.exports = () => new Promise((resolve, reject) => {
+    global.unfetter = global.unfetter || {};
+    if (global.unfetter.conn === undefined) {
         mongoose.connect(`mongodb://${process.env.MONGO_REPOSITORY}:${process.env.MONGO_PORT}/stix`, {
             server: {
-                poolSize: 5,
+                poolSize: 12,
                 reconnectTries: 100,
                 socketOptions: {
                     keepAlive: 300000,
@@ -18,15 +48,24 @@ module.exports = () => {
             }
         });
 
-        const db = global.conn = mongoose.connection;
-        db.on('error', console.error.bind(console, 'connection error:'));
-        db.on('connected', () => console.log('connected to mongodb'));
+        global.unfetter.conn = mongoose.connection;
+        const db = global.unfetter.conn;
+        db.on('error', () => {
+            console.error(console, 'connection error:');
+            reject('error connecting to monogo');
+        });
+        db.on('connected', () => {
+            console.log('connected to mongodb');
+            lookupGlobalValues();
+            resolve(global.unfetter.conn);
+        });
         db.on('disconnected', () => console.log('disconnected from mongodb'));
-        process.on('SIGINT', function () {
-            db.close(function () {
+        process.on('SIGINT', () => {
+            db.close(() => {
                 console.log('Safely closed MongoDB Connection');
                 process.exit(0);
             });
         });
-    }     
-};
+    }
+    resolve(global.unfetter.conn);
+});
