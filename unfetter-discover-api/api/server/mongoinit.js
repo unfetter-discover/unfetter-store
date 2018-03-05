25b4b7ef443e47938104858bdde41cac268c894f
mushinlogit <mushinlogit@gmail.com>
2018-03-05 09:34:05 -0500

    load values on startup, better lint (#102)

--- unfetter-discover-api/api/server/mongoinit.js
+++ unfetter-discover-api/api/server/mongoinit.js
@@ -5,23 +5,38 @@ process.env.MONGO_DBNAME = process.env.MONGO_DBNAME || 'stix';
 const modelFactory = require('../controllers/shared/modelFactory');
 const mongoose = require('mongoose');
 
-const utilityModel = require('../models/utility');
-
 const identityModel = modelFactory.getModel('identity');
-const configModel = modelFactory.getModel('config');
 
-const PROCESSOR_STATUS_ID = process.env.PROCESSOR_STATUS_ID || 'f09ad23d-c9f7-40a3-8afa-d9560e6df95b';
-// The maximum amount of tries mongo will attempt to get processor Status
-const MAX_GET_PROCESSOR_STATUS_ATTEMPTS = process.env.MAX_GET_PROCESSOR_STATUS_ATTEMPTS || 10;
-// The amount of time between each connection attempt in ms
-const GET_PROCESSOR_RETRY_TIME = process.env.GET_PROCESSOR_RETRY_TIME || 5000;
-
-const mongoDebug = process.env.MONGO_DEBUG || false;
+const mongoDebug = process.env.MONGO_DEBUG || true;
 mongoose.set('debug', mongoDebug);
 mongoose.Promise = global.Promise;
 
-module.exports = () => {
-    if (global.conn === undefined) {
+/**
+ * @description populate global lookup values
+ */
+const lookupGlobalValues = () => {
+    console.log('looking up global values...');
+    if (global.unfetter.identities === undefined) {
+        identityModel
+            .findOne({ 'stix.type': 'identity', 'stix.name': 'Unfetter Open' })
+            .exec((err, result) => {
+                if (err) {
+                    return;
+                }
+
+                const openIdent = { ...result };
+                global.unfetter.identities = [
+                    ...(global.unfetter.identities || []),
+                    openIdent,
+                ];
+                global.unfetter.openIdentity = openIdent;
+                console.log('set open identity with id, ', openIdent._id || '');
+            });
+    }
+};
+module.exports = () => new Promise((resolve, reject) => {
+    global.unfetter = global.unfetter || {};
+    if (global.unfetter.conn === undefined) {
         mongoose.connect(`mongodb://${process.env.MONGO_REPOSITORY}:${process.env.MONGO_PORT}/stix`, {
             server: {
                 poolSize: 12,
@@ -37,17 +52,12 @@ module.exports = () => {
         const db = global.unfetter.conn;
         db.on('error', () => {
             console.error(console, 'connection error:');
-            reject(new Error('error connecting to mongo'));
+            reject('error connecting to monogo');
         });
         db.on('connected', () => {
             console.log('connected to mongodb');
-            getProcessorStatus()
-                .then(_ => { // eslint-disable-line no-unused-vars
-                    lookupGlobalValues()
-                        .then(__ => resolve('Mongo DB running')) // eslint-disable-line no-unused-vars
-                        .catch(errMsg => reject(errMsg));
-                })
-                .catch(err => reject(err));
+            lookupGlobalValues();
+            resolve(global.unfetter.conn);
         });
         db.on('disconnected', () => console.log('disconnected from mongodb'));
         process.on('SIGINT', () => {
@@ -57,4 +67,5 @@ module.exports = () => {
             });
         });
     }
+    resolve(global.unfetter.conn);
 });
--- unfetter-discover-api/api/server/serverinit.js
+++ unfetter-discover-api/api/server/serverinit.js
@@ -9,7 +9,8 @@ const onListening = (server, resolveCallBack) => {
     const addr = server.address();
     const bind = typeof addr === 'string' ?
         `pipe ${addr}` : `port ${addr.port}`;
-    resolveCallBack(`Server listening on ${bind}`);
+    console.log(`Listening on ${bind}`);
+    resolveCallBack(global.unfetter.httpServer);
 };
 
 const onError = (error, rejectCallBack) => {
@@ -22,16 +23,16 @@ const onError = (error, rejectCallBack) => {
 
     // handle specific listen errors with friendly messages
     switch (error.code) {
-    case 'EACCES':
-        console.error(`${bind} requires elevated privileges`);
-        process.exit(1);
-        break;
-    case 'EADDRINUSE':
-        console.error(`${bind} is already in use`);
-        process.exit(1);
-        break;
-    default:
-        throw error;
+        case 'EACCES':
+            console.error(`${bind} requires elevated privileges`);
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(`${bind} is already in use`);
+            process.exit(1);
+            break;
+        default:
+            throw error;
     }
     rejectCallBack(error.code);
 };
@@ -45,8 +46,9 @@ module.exports = () => new Promise((resolve, reject) => {
             cert: fs.readFileSync('/etc/pki/tls/certs/server.crt')
         }, app);
         server.listen(port);
-        server.on('error', error => onError(error, reject));
+        server.on('error', (error) => onError(error, reject));
         server.on('listening', () => onListening(server, resolve));
         global.unfetter.httpServer = server;
     }
+    resolve(global.unfetter.httpServer);
 });
