2b6c59d39d6d06b033e394f266b3932630a7f899
mushinlogit <mushinlogit@gmail.com>
2018-03-05 09:34:05 -0500

    load values on startup, better lint (#102)

--- unfetter-discover-api/api/server/server.js
+++ unfetter-discover-api/api/server/server.js
@@ -1,50 +1,16 @@
 process.env.RUN_MODE = process.env.RUN_MODE || 'DEMO';
-const port = process.env.PORT || '3000';
+const mongoinit = require('./mongoinit.js');
+const serverinit = require('./serverinit.js');
 
-const app = require('../../app');
-const fs = require('fs');
-const spdy = require('spdy');
-const mongoinit = require('./mongoinit.js')();
+const err =
+  (errMsg) => {
+    console.log('Failed to start http server, giving up', errMsg);
+  };
 
-app.set('port', port);
+global.unfetter = global.unfetter || {};
 
-const server = spdy.createServer({
-  key: fs.readFileSync('/etc/pki/tls/certs/server.key'),
-  cert: fs.readFileSync('/etc/pki/tls/certs/server.crt')
-}, app);
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string' ?
-    'Pipe ' + port :
-    'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string' ?
-    'pipe ' + addr :
-    'port ' + addr.port;
-  console.log('Listening on ' + bind);
-}
+mongoinit()
+  .then(async () => {
+    console.log('connected to mongodb, starting api http server...');
+    await serverinit().catch(err);
+  }).catch(err);
---
+++ unfetter-discover-api/api/server/serverinit.js
@@ -0,0 +1,54 @@
+const port = process.env.PORT || '3000';
+
+const app = require('../../app');
+const fs = require('fs');
+const spdy = require('spdy');
+
+
+const onListening = (server, resolveCallBack) => {
+    const addr = server.address();
+    const bind = typeof addr === 'string' ?
+        `pipe ${addr}` : `port ${addr.port}`;
+    console.log(`Listening on ${bind}`);
+    resolveCallBack(global.unfetter.httpServer);
+};
+
+const onError = (error, rejectCallBack) => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    const bind = typeof port === 'string' ?
+        `Pipe ${port}` : `Port ${port}`;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(`${bind} requires elevated privileges`);
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(`${bind} is already in use`);
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+    rejectCallBack(error.code);
+};
+
+module.exports = () => new Promise((resolve, reject) => {
+    global.unfetter = global.unfetter || {};
+    if (global.unfetter.httpServer === undefined) {
+        app.set('port', port);
+        const server = spdy.createServer({
+            key: fs.readFileSync('/etc/pki/tls/certs/server.key'),
+            cert: fs.readFileSync('/etc/pki/tls/certs/server.crt')
+        }, app);
+        server.listen(port);
+        server.on('error', (error) => onError(error, reject));
+        server.on('listening', () => onListening(server, resolve));
+        global.unfetter.httpServer = server;
+    }
+    resolve(global.unfetter.httpServer);
+});
