c59cbec0f299556366d54bd25734fe4a3c5332a8
j987987 <30236110+j987987@users.noreply.github.com>
2018-03-08 07:04:30 -0500

    added token refreshing to swagger api (#106)

--- unfetter-api-explorer/src/js/app.js
+++ unfetter-api-explorer/src/js/app.js
@@ -3,18 +3,67 @@ import { SwaggerUIBundle, SwaggerUIStandalonePreset } from 'swagger-ui-dist';
 
 // ~~~ Init SCSS ~~~
 import '../scss/main.scss';
+import { refreshToken } from './refresh-token';
+import { getConfig } from './get-config';
 
 const mainEl = 'main';
+// Grace period between attempts to refresh JWT token
+const refreshBuffer = 0.3;
+
+function showErrorCard(title, body) {
+    const el = document.getElementById(mainEl);
+    el.innerHTML = `
+        <div class="uf-container">
+            <br><br>
+            <div class="uf-error-card">
+                <h4>Error: ${title}</h4>
+                <p class="uf-well-warn">${body}</p>
+            </div>
+        </div>
+    `;
+}
 
 (() => {
-    window.onload = function () {
-
+    window.onload = function () {       
         const urlParams = new URLSearchParams(window.location.search);
 
         if (urlParams.has('token')) {
 
-            const token = urlParams.get('token');
-            // TODO verify token
+            let token = urlParams.get('token');
+            let tokenRefreshMs = 10000;
+            refreshToken(token)
+                .then((response) => {
+                    if (response.data.data && response.data.data.attributes && response.data.data.attributes.token) {
+                        token = response.data.data.attributes.token;
+                        getConfig(token)
+                            .then((config) => {
+                                if (config.data && config.data.data && config.data.data.length && config.data.data[0].attributes && config.data.data[0].attributes.configValue) {
+                                    tokenRefreshMs = config.data.data[0].attributes.configValue * 1000;
+                                    tokenRefreshMs = Math.floor(tokenRefreshMs - (refreshBuffer * tokenRefreshMs));
+                                    setInterval(() => {
+                                        refreshToken(token)
+                                            .then((response) => {
+                                                if (response.data.data && response.data.data.attributes && response.data.data.attributes.token) {
+                                                    console.log('Token refreshed');
+                                                    token = response.data.data.attributes.token;
+                                                } else {
+                                                    showErrorCard('Unable to Refresh Token', 'Reopen API Documentation from Unfetter.');
+                                                }
+                                            })
+                                    }, tokenRefreshMs);
+                                }
+                                console.log('CONFIG~~~~', config);
+                            })
+                            .catch((err) => {
+                                showErrorCard('Unable to Get Configurations', 'This instance of Unfetter is likely misconfigured.  Contact an admin.');
+                            });
+                    } else {
+                        showErrorCard('Unable to Refresh Token', 'Reopen API Documentation from Unfetter.');
+                    }
+                })
+                .catch((err) => {
+                    showErrorCard('Unable to Refresh Token', 'Reopen API Documentation from Unfetter.');
+                });
 
             window['SwaggerUIBundle'] = window['swagger-ui-bundle']
             window['SwaggerUIStandalonePreset'] = window['swagger-ui-standalone-preset']
@@ -39,17 +88,7 @@ const mainEl = 'main';
             window.ui = ui;
             
         } else {
-            // No token present
-            const el = document.getElementById(mainEl);
-            el.innerHTML = `
-                <div class="uf-container">
-                    <br><br>
-                    <div class="uf-error-card">
-                        <h4>Error: You Must Enter Through Unfetter</h4>
-                        <p class="uf-well-warn">To enter this documentation site, you must authenticate to the Unfetter user interface and use the <strong>API Documentation</strong> in the application menu.</p>
-                    </div>
-                </div>
-            `;
+            showErrorCard('You Must Enter Through Unfetter', 'To enter this documentation site, you must authenticate to the Unfetter user interface and use the <strong>API Documentation</strong> in the application menu.');
         }
     }
 })();
