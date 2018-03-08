0a205cfc7785fce7349698531c107b82604b30b3
j987987 <30236110+j987987@users.noreply.github.com>
2018-03-08 07:04:30 -0500

    added token refreshing to swagger api (#106)

--- unfetter-api-explorer/src/js/app.js
+++ unfetter-api-explorer/src/js/app.js
@@ -10,7 +10,6 @@ const urlParams = new URLSearchParams(window.location.search);
 const mainEl = 'main';
 // Grace period between attempts to refresh JWT token
 const refreshBuffer = 0.3;
-let token = '';
 
 function showErrorCard(title, body) {
     const el = document.getElementById(mainEl);
@@ -25,59 +24,72 @@ function showErrorCard(title, body) {
     `;
 }
 
-function initToken() {
-    token = urlParams.get('token');
-    let tokenRefreshMs = 10000;
-    refreshToken(token)
-        .then((response) => {
-            if (response.data.data && response.data.data.attributes && response.data.data.attributes.token) {
-                token = response.data.data.attributes.token;
-                getConfig(token)
-                    .then((config) => {
-                        if (config.data && config.data.data && config.data.data.length && config.data.data[0].attributes && config.data.data[0].attributes.configValue) {
-                            tokenRefreshMs = config.data.data[0].attributes.configValue * 1000;
-                            tokenRefreshMs = Math.floor(tokenRefreshMs - (refreshBuffer * tokenRefreshMs));
-                            setInterval(() => {
-                                refreshToken(token)
-                                    .then((response) => {
-                                        if (response.data.data && response.data.data.attributes && response.data.data.attributes.token) {
-                                            console.log('Token refreshed');
-                                            token = response.data.data.attributes.token;
-                                        } else {
-                                            showErrorCard('Unable to Refresh Token', 'Reopen API Explorer from Unfetter.');
-                                        }
-                                    })
-                            }, tokenRefreshMs);
-                        }
-                    })
-                    .catch((err) => {
-                        showErrorCard('Unable to Get Configurations', 'This instance of Unfetter is likely misconfigured.  Contact an admin.');
-                    });
-            } else {
-                showErrorCard('Unable to Refresh Token', 'Reopen API Explorer from Unfetter.');
-            }
-        })
-        .catch((err) => {
-            showErrorCard('Unable to Refresh Token', 'Reopen API Explorer from Unfetter.');
-        });
-
-    
-}
-
 (() => {
-    window.onload = function () {     
-        const RUN_MODE = urlParams.get('RUN_MODE');
-        
-        if (RUN_MODE && RUN_MODE === 'DEMO') {
-            console.log('Starting in DEMO mode');
-        } else {
-            console.log('Starting in UAC mode');     
+    window.onload = function () {       
+        const urlParams = new URLSearchParams(window.location.search);
 
-            if (urlParams.has('token')) {
-                initToken();
-            } else {
-                showErrorCard('You Must Enter Through Unfetter', 'To enter this documentation site, you must authenticate to the Unfetter user interface and use the <strong>API Explorer</strong> link in the application menu.');
-            }
+        if (urlParams.has('token')) {
+
+            let token = urlParams.get('token');
+            let tokenRefreshMs = 10000;
+            refreshToken(token)
+                .then((response) => {
+                    if (response.data.data && response.data.data.attributes && response.data.data.attributes.token) {
+                        token = response.data.data.attributes.token;
+                        getConfig(token)
+                            .then((config) => {
+                                if (config.data && config.data.data && config.data.data.length && config.data.data[0].attributes && config.data.data[0].attributes.configValue) {
+                                    tokenRefreshMs = config.data.data[0].attributes.configValue * 1000;
+                                    tokenRefreshMs = Math.floor(tokenRefreshMs - (refreshBuffer * tokenRefreshMs));
+                                    setInterval(() => {
+                                        refreshToken(token)
+                                            .then((response) => {
+                                                if (response.data.data && response.data.data.attributes && response.data.data.attributes.token) {
+                                                    console.log('Token refreshed');
+                                                    token = response.data.data.attributes.token;
+                                                } else {
+                                                    showErrorCard('Unable to Refresh Token', 'Reopen API Documentation from Unfetter.');
+                                                }
+                                            })
+                                    }, tokenRefreshMs);
+                                }
+                                console.log('CONFIG~~~~', config);
+                            })
+                            .catch((err) => {
+                                showErrorCard('Unable to Get Configurations', 'This instance of Unfetter is likely misconfigured.  Contact an admin.');
+                            });
+                    } else {
+                        showErrorCard('Unable to Refresh Token', 'Reopen API Documentation from Unfetter.');
+                    }
+                })
+                .catch((err) => {
+                    showErrorCard('Unable to Refresh Token', 'Reopen API Documentation from Unfetter.');
+                });
+
+            window['SwaggerUIBundle'] = window['swagger-ui-bundle']
+            window['SwaggerUIStandalonePreset'] = window['swagger-ui-standalone-preset']
+            
+            const ui = SwaggerUIBundle({
+                url: '/explorer/assets/swagger.json',
+                dom_id: `#${mainEl}`,
+                deepLinking: true,
+                presets: [
+                    SwaggerUIBundle.presets.apis,
+                    SwaggerUIStandalonePreset
+                ],
+                plugins: [
+                    SwaggerUIBundle.plugins.DownloadUrl
+                ],
+                layout: 'StandaloneLayout',
+                requestInterceptor: (req) => {
+                    req.headers.authorization = token;
+                    return req;
+                }
+            });
+            window.ui = ui;
+            
+        } else {
+            showErrorCard('You Must Enter Through Unfetter', 'To enter this documentation site, you must authenticate to the Unfetter user interface and use the <strong>API Documentation</strong> in the application menu.');
         }
 
         window['SwaggerUIBundle'] = window['swagger-ui-bundle'];
