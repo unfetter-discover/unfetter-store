6ceb62b8b3c8f5360b80b8ae8a29ad888e4b7e80
Joe D'Amato <jdamatomitre.org>
2018-03-16 10:01:10 -0400

    Added mongoose models for assessments 3.0

---
+++ unfetter-discover-api/api/models/x-unfetter-object-assessment.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose');
+const stixCommons = require('./stix-commons');
+
+const scoreVals = ['S', 'M', 'L', 'N/A', 'N'];
+
+const StixSchema = {
+    type: {
+        type: String,
+        enum: ['x-unfetter-capability'],
+        default: 'x-unfetter-capability'
+    },
+    id: String,
+    created_by_ref: {
+        type: String,
+        required: [true, 'created_by_ref is required']
+    },
+    name: {
+        type: String,
+        required: [true, 'name is required']
+    },
+    description: String,
+    object_ref: {
+        type: String,
+        required: [true, 'object_ref is required']
+    },
+    is_baseline: {
+        type: Boolean,
+        required: [true, 'is_baseline is required']
+    },
+    set_ref: [String],
+    assessment_objects: [{
+        assessed_object_ref: String,
+        questions: [
+            {
+                name: {
+                    type: String,
+                    enum: ['protect'],
+                    default: 'protect',
+                },
+                score: {
+                    type: String,
+                    enum: scoreVals,
+                    default: null
+                }
+            },
+            {
+                name: {
+                    type: String,
+                    enum: ['detect'],
+                    default: 'detect',
+                },
+                score: {
+                    type: String,
+                    enum: scoreVals,
+                    default: null
+                }
+            },
+            {
+                name: {
+                    type: String,
+                    enum: ['respond'],
+                    default: 'respond',
+                },
+                score: {
+                    type: String,
+                    enum: scoreVals,
+                    default: null
+                }
+            }
+        ]
+    }]
+};
+
+const objectAssessment = mongoose.model('XUnfetterObjectAssessment', stixCommons['makeSchema'](StixSchema), 'stix');
+
+module.exports = objectAssessment;
