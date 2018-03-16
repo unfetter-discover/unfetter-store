a7e0abb74106b9d4d6dc8e7527c78f67915e9574
Joe D'Amato <jdamatomitre.org>
2018-03-16 10:01:10 -0400

    Added mongoose models for assessments 3.0

---
+++ unfetter-discover-api/api/models/x-unfetter-assessment-set.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const stixCommons = require('./stix-commons');
+
+const StixSchema = {
+    type: {
+        type: String,
+        enum: ['x-unfetter-assessment-set'],
+        default: 'x-unfetter-assessment-set'
+    },
+    id: String,
+    created_by_ref: {
+        type: String,
+        required: [true, 'created_by_ref is required']
+    },
+    name: {
+        type: String,
+        required: [true, 'name is required']
+    },
+    description: String,
+    assessment_group_ref: String,
+    assessments: [String]
+};
+
+const assessmentSet = mongoose.model('XUnfetterAssessmentSet', stixCommons['makeSchema'](StixSchema), 'stix');
+
+module.exports = assessmentSet;
---
+++ unfetter-discover-api/api/models/x-unfetter-capability.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const stixCommons = require('./stix-commons');
+
+const StixSchema = {
+    type: {
+        type: String,
+        enum: ['x-unfetter-capability'],
+        default: 'x-unfetter-capability'
+    },
+    id: String,
+    created_by_ref: {
+        type: String,
+        required: [true, 'created_by_ref is required']
+    },
+    name: {
+        type: String,
+        required: [true, 'name is required']
+    },
+    description: String,
+    category: String
+};
+
+const capability = mongoose.model('XUnfetterCapability', stixCommons['makeSchema'](StixSchema), 'stix');
+
+module.exports = capability;
