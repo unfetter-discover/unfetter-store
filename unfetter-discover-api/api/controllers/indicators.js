cca8cbd7dae0c72ca8494e483e79faf289fea817
j987987 <30236110+j987987@users.noreply.github.com>
2018-03-20 12:57:26 -0400

    added summary stats route (#121)

--- unfetter-discover-api/api/controllers/indicators.js
+++ unfetter-discover-api/api/controllers/indicators.js
@@ -153,149 +153,84 @@ const summaryStatistics = (req, res) => {
     });
 };
 
-const search = (req, res) => {
-    const searchParameters = req.swagger.params.searchparameters && req.swagger.params.searchparameters.value ? JSON.parse(req.swagger.params.searchparameters.value) : null;
-    const sorttype = req.swagger.params.sorttype && req.swagger.params.sorttype.value ? req.swagger.params.sorttype.value : 'NEWEST';
-    const sortObj = {};
-
-    // Database sorts
-    switch (sorttype) {
-    case 'NEWEST':
-        sortObj['stix.created'] = -1;
-        break;
-    case 'OLDEST':
-        sortObj['stix.created'] = 1;
-        break;
-    default:
-        break;
-    }
-    if (searchParameters) {
-        const promises = [];
-
-        // Get filtered indicators
-        const filterObj = { 'stix.type': 'indicator' };
-        if (searchParameters.indicatorName && searchParameters.indicatorName !== '') {
-            filterObj['stix.name'] = { $regex: `.*${searchParameters.indicatorName.replace(/[-[\]{}()*+?.,\\^$|#]/g, '\\$&')}.*`, $options: 'i' };
-        }
-        if (searchParameters.labels && searchParameters.labels.length) {
-            filterObj['stix.labels'] = { $in: searchParameters.labels };
-        }
-        if (searchParameters.organizations && searchParameters.organizations.length) {
-            filterObj['stix.created_by_ref'] = { $in: searchParameters.organizations };
-        }
-        if (searchParameters.killChainPhases && searchParameters.killChainPhases.length) {
-            filterObj['stix.kill_chain_phases.phase_name'] = { $in: searchParameters.killChainPhases };
-        }
-        if (searchParameters.published && searchParameters.published.length) {
-            // Mapping is there because mat-option insists on giving a string
-            filterObj['metaProperties.published'] = { $in: searchParameters.published.map(p => p === 'true') };
-        }
-
-        promises.push(stixModel.find(SecurityHelper.applySecurityFilter(filterObj, req.user)).sort(sortObj).exec());
-
-        // Get relationships of attack patterns if in params, or resolve []
-        if (searchParameters.attackPatterns && searchParameters.attackPatterns.length) {
-            const apFilter = {
-                'stix.type': 'relationship',
-                'stix.relationship_type': 'indicates',
-                'stix.target_ref': {
-                    $in: searchParameters.attackPatterns
+const summaryStatistics = (req, res) => {
+    const query = [{
+            $match: {
+                'stix.type': 'indicator'
+            }
+        },
+        {
+            $project: {
+                created_by_ref: '$stix.created_by_ref',
+                views: {
+                    $cond: [{
+                            $gt: ['$metaProperties.interactions', null]
+                        },
+                        {
+                            '$size': '$metaProperties.interactions'
+                        },
+                        0
+                    ]
+                },
+                likes: {
+                    $cond: [{
+                            $gt: ['$metaProperties.likes', null]
+                        },
+                        {
+                            '$size': '$metaProperties.likes'
+                        },
+                        0
+                    ]
+                },
+                comments: {
+                    $cond: [{
+                            $gt: ['$metaProperties.comments', null]
+                        },
+                        {
+                            '$size': '$metaProperties.comments'
+                        },
+                        0
+                    ]
                 }
-            };
-            promises.push(stixModel.find(apFilter).select({ 'stix.source_ref': 1 }).exec());
-        } else {
-            promises.push(Promise.resolve([]));
+            }
+        },
+        {
+            $group: {
+                _id: '$created_by_ref',
+                count: {
+                    $sum: 1
+                },
+                views: {
+                    $sum: '$views'
+                },
+                likes: {
+                    $sum: '$likes'
+                },
+                comments: {
+                    $sum: '$comments'
+                }
+            }
         }
+    ];
 
-        // Get sensors if in params, or resovle []
-        if (searchParameters.sensors && searchParameters.sensors.length) {
-            const sensorFilter = {
-                'stix.type': 'x-unfetter-sensor',
-                'metaProperties.observedData': { $exists: 1 },
-                _id: { $in: searchParameters.sensors }
-            };
-            promises.push(stixModel.find(SecurityHelper.applySecurityFilter(sensorFilter, req.user)).select({ 'metaProperties.observedData': 1 }).exec());
-        } else {
-            promises.push(Promise.resolve([]));
-        }
-
-        // TODO get sensors, or determine if it should be done client side
-
-        Promise.all(promises)
-            .then(([indicatorsRes, apRelationshipsRes, sensorsRes]) => {
-                let indicators = dataHelper.getEnhancedData(indicatorsRes, req.swagger.params);
-
-                // Server side filter of attack patterns
-                if (searchParameters.attackPatterns && searchParameters.attackPatterns.length) {
-                    if (apRelationshipsRes.length) {
-                        const indicatorsRelatedToAps = apRelationshipsRes
-                            .map(apRel => apRel.toObject())
-                            .map(apRel => apRel.stix.source_ref);
-
-                        indicators = indicators.filter(indicator => indicatorsRelatedToAps.includes(indicator.id));
-                    } else {
-                        // No relationships found, assign empty array
-                        indicators = [];
-                    }
-                }
-
-                // Server side filter of sensors
-                if (searchParameters.sensors && searchParameters.sensors.length && sensorsRes.length) {
-                    const sensors = sensorsRes.map(sensor => sensor.toObject());
-                    indicators = indicators
-                        .filter(indicator => indicator.metaProperties && indicator.metaProperties.observedData)
-                        .filter(indicator =>
-                            sensors.filter(sensor => {
-                                let retVal = true;
-                                indicator.metaProperties.observedData.forEach(obsData => {
-                                    let sensorMatch = false;
-                                    sensor.metaProperties.observedData.forEach(sensorObsData => {
-                                        if (sensorObsData.name === obsData.name && sensorObsData.action === obsData.action && sensorObsData.property === obsData.property) {
-                                            sensorMatch = true;
-                                        }
-                                    });
-                                    if (!sensorMatch) {
-                                        retVal = false;
-                                    }
-                                });
-                                return retVal;
-                            }).length > 0
-                        );
-                }
-
-                // Server side sorts (can't be done easily in mongo)
-                switch (sorttype) {
-                case 'LIKES':
-                    indicators = indicators.sort((a, b) => sortArrayLength(a, b, 'likes'));
-                    break;
-                case 'COMMENTS':
-                    indicators = indicators.sort((a, b) => sortArrayLength(a, b, 'comments'));
-                    break;
-                default:
-                    break;
-                }
-                const requestedUrl = apiRoot + req.originalUrl;
-                const convertedResult = jsonApiConverter.convertJsonToJsonApi(indicators, 'indicator', requestedUrl);
-                return res.json({ data: convertedResult, links: { self: requestedUrl } });
-            })
-            .catch(err => {
-                res.status(500).json({
-                    errors: [{
-                        status: 500,
-                        source: '',
-                        title: 'Error',
-                        code: '',
-                        detail: `An error has occured: ${err}`
-                    }]
-                });
+    aggregationModel.aggregate(query, (err, results) => {
+        if (err) {
+            return res.status(500).json({
+                errors: [{
+                    status: 500,
+                    source: '',
+                    title: 'Error',
+                    code: '',
+                    detail: 'An unknown error has occurred.'
+                }]
             });
-    } else {
-        return res.status(400).json({
-            errors: [{
-                status: 500, source: '', title: 'Error', code: '', detail: 'Search parameters are required.'
-            }]
+        }
+        return res.json({
+            data: {
+                attributes: results
+            }
         });
-    }
+    });
 };
 
 module.exports = {
@@ -304,5 +239,6 @@ module.exports = {
     add: controller.add(),
     update: controller.update(),
     deleteById: controller.deleteById(),
-    attackPatternsByIndicator
+    attackPatternsByIndicator,
+    summaryStatistics
 };
