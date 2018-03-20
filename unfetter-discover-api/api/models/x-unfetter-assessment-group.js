9de18e4883441b93de2a46fddab8f47d42d9f897
Shawn Bertrand <34275439+sbertrandmitre@users.noreply.github.com>
2018-03-20 12:16:11 -0400

    Issue 661 (#120)
    * Remove spaces in example text to avoid bad quote conversion
    
    * Added mongoose models for assessments 3.0
    
    * Added (some of the) swagger yaml files for capabilities. No controller exists yet.
    
    * Added get controller for capabilities
    
    * Introduced getById, add, update, and delete, but only tests for get for now; next commit will have full tests
    
    * Added mongoose models for assessments 3.0
    
    * Added (some of the) swagger yaml files for capabilities. No controller exists yet.
    
    * Added get controller for capabilities
    
    * Introduced getById, add, update, and delete, but only tests for get for now; next commit will have full tests
    
    * Removed console.log statements

---
+++ unfetter-discover-api/api/models/x-unfetter-assessment-group.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const stixCommons = require('./stix-commons');
+
+const StixSchema = {
+    type: {
+        type: String,
+        enum: ['x-unfetter-assessment-group'],
+        default: 'x-unfetter-assessment-group'
+    },
+    id: String,
+    created_by_ref: {
+        type: String,
+        required: [true, 'created_by_ref is required']
+    },
+    name: {
+        type: String,
+        required: [true, 'name is required']
+    },
+    description: String,
+    object_refs: [String],
+    assessment_sets: [String]
+};
+
+const assessmentGroup = mongoose.model('XUnfetterAssessmentGroup', stixCommons['makeSchema'](StixSchema), 'stix');
+
+module.exports = assessmentGroup;
