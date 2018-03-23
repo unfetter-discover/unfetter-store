4206f0eba911de5f99b81b9decc297fac7b4c329
j987987 <j987897@users.noreply.github.com>
2018-03-23 12:52:36 -0400

    added sensors relationships to basecontroller

--- unfetter-discover-api/api/controllers/shared/basecontroller.js
+++ unfetter-discover-api/api/controllers/shared/basecontroller.js
@@ -231,6 +231,9 @@ module.exports = class BaseController {
                         case 'indicator':
                             relType = 'indicates';
                             break;
+                        case 'x-unfetter-sensor':
+                            relType = 'detects';
+                            break;
                         default:
                         }
                         const tempRelationship = {
