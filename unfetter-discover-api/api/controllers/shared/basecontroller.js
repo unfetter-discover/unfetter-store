bbad60a510a64c908b4d0f4fca518b2509284c48
Joe D'Amato <jdamatomitre.org>
2018-03-23 09:01:16 -0400

    Removed debug console.log statements

--- unfetter-discover-api/api/controllers/shared/basecontroller.js
+++ unfetter-discover-api/api/controllers/shared/basecontroller.js
@@ -86,8 +86,6 @@ module.exports = class BaseController {
 
                     const requestedUrl = apiRoot + req.originalUrl;
                     const data = DataHelper.getEnhancedData(result, req.swagger.params);
-                    console.log('eeeeeeeee');
-                    console.log(data);
                     const convertedResult = jsonApiConverter.convertJsonToJsonApi(data, type, requestedUrl);
                     // return res.status(200).json({ links: { self: requestedUrl, }, data: convertedResult });
                     callback(err, convertedResult, requestedUrl, req, res);
@@ -145,8 +143,6 @@ module.exports = class BaseController {
         return (req, res) => {
             res.header('Content-Type', 'application/vnd.api+json');
             const obj = {};
-
-            console.log(`&&&&&&&& Add value data is:  ${req.swagger.params.data.value.data}`);
 
             if (req.swagger.params.data !== undefined && req.swagger.params.data.value.data.attributes !== undefined) {
                 const data = req.swagger.params.data.value.data;
