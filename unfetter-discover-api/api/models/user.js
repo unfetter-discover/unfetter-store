bbb1ff39e37e983295320b21774d655b780a5e45
j987987 <30236110+j987987@users.noreply.github.com>
2018-03-27 17:10:21 -0400

    Issue 839 (#133)
    * add public config api
    
    * fixed bug with isdefined function
    
    * added registration info to mongoose model

--- unfetter-discover-api/api/models/user.js
+++ unfetter-discover-api/api/models/user.js
@@ -93,37 +93,18 @@ const UserSchema = new mongoose.Schema({
         type: Date,
         default: Date.now,
         required: [true, 'created is required']
+    },
+    registrationInformation: {
+        applicationNote: {
+            type: String 
+        },
+        requestedOrganization: {
+            type: String 
+        }
     }
 });
 
 UserSchema.index({ userName: 1 });
 UserSchema.index({ role: 1 });
 
-module.exports = mongoose.model('User', UserSchema, 'user');
-// const User = module.exports;
-
-// module.exports.getUserById = function (id, callback) {
-//     User.findById(id, callback);
-// }
-
-// module.exports.getUserByUsername = function (username, callback) {
-//     const query = { username: username }
-//     User.findOne(query, callback);
-// }
-
-// module.exports.addUser = function (newUser, callback) {
-//     bcrypt.genSalt(10, (err, salt) => {
-//         bcrypt.hash(newUser.password, salt, (err, hash) => {
-//             if (err) throw err;
-//             newUser.password = hash;
-//             newUser.save(callback);
-//         });
-//     });
-// }
-
-// module.exports.comparePassword = function (candidatePassword, hash, callback) {
-//     bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
-//         if (err) throw err;
-//         callback(null, isMatch);
-//     });
-// }
+const User = module.exports = mongoose.model('User', UserSchema, 'user');
