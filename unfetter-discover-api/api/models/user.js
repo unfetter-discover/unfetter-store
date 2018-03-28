1a986397eb785642d8f10d8e5414b200e4bb104c
mpmmasterpeace <24652622+mpmmasterpeace@users.noreply.github.com>
2018-03-28 06:53:38 -0400

    attempt to keep linting clean and get a clean travis build

--- unfetter-discover-api/api/models/user.js
+++ unfetter-discover-api/api/models/user.js
@@ -109,29 +109,3 @@ UserSchema.index({ role: 1 });
 
 module.exports = mongoose.model('User', UserSchema, 'user');
 // const User = module.exports;
-
-// module.exports.getUserById = function (id, callback) {
-//     User.findById(id, callback);
-// }
-
-// module.exports.getUserByUsername = function (username, callback) {
-//     const query = { username: username }
-//     User.findOne(query, callback);
-// }
-
-// module.exports.addUser = function (newUser, callback) {
-//     bcrypt.genSalt(10, (err, salt) => {
-//         bcrypt.hash(newUser.password, salt, (err, hash) => {
-//             if (err) throw err;
-//             newUser.password = hash;
-//             newUser.save(callback);
-//         });
-//     });
-// }
-
-// module.exports.comparePassword = function (candidatePassword, hash, callback) {
-//     bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
-//         if (err) throw err;
-//         callback(null, isMatch);
-//     });
-// }
