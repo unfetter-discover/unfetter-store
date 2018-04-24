b085e841c9539ab920d36ac4a37c48c38aa99a51
j987987 <j987897@users.noreply.github.com>
2018-04-24 12:24:31 -0400

    broken test comment

---
+++ unfetter-discover-processor/src/services/unfetter-updater.service.spec.ts
@@ -0,0 +1,66 @@
+import UnfetterUpdaterService from './unfetter-updater.service';
+import MongooseModels from '../models/mongoose-models';
+
+describe('UnfetterUpdaterService', () => {
+
+    // beforeEach(() => {
+    //     const mockStixModel = {
+    //         findByIdAndUpdate: () => {
+    //             console.log(')(#I(I#% IN MOCK');
+    //             return {};
+    //         },
+    //         exec: () => {
+    //             console.log('IJIJ MOCK');
+    //             return Promise.resolve(true);
+    //         },
+    //         find: () => {
+    //             console.log('IN FIND');
+    //         }
+    //     }
+    //     spyOn(MongooseModels, 'stixModel').and.returnValue(mockStixModel);
+    //     // console.log(Object.keys(MongooseModels.stixModel));
+    //     console.log(UnfetterUpdaterService);
+    // });
+
+    // describe('generateUpdates', () => {
+    //     const now = new Date();
+    //     it('should return IDs matching updates', async (done) => {
+    //         const mockStix = [
+    //             {
+    //                 _id: '1234',
+    //                 extendedProperties: {
+    //                     foo: 'bar'
+    //                 }
+    //             },
+    //             {
+    //                 _id: '5678',
+    //                 metaProperties: {
+    //                     foo: 'bar'
+    //                 }
+    //             }
+    //         ];
+    //         const res = await UnfetterUpdaterService.generateUpdates(mockStix);
+    //         console.log('$$$$', res);
+    //         done();
+    //     });
+    // });
+
+    describe('removeUpdateDocs', () => {
+        it('should remove docs tagged for update', () => {
+            const mockStix = [
+                { _id: '12' },
+                { _id: '34' },          
+                { _id: '56' },          
+                { _id: '78' },          
+                { _id: '90' }          
+            ];
+            const mockUpdateIds = [ '34', '78' ];
+            UnfetterUpdaterService.removeUpdateDocs(mockStix, mockUpdateIds);
+            expect(mockStix.length).toBe(3);
+            const idString = mockStix
+                .map((s) => s._id)
+                .join('');
+            expect(idString).toBe('125690');
+        });        
+    });
+});
