bf1ed12701280297884c29c8e813375211b242ea
webiator <6414732+webiator@users.noreply.github.com>
2018-04-24 11:23:29 -0400

    Fixes linting errors

--- unfetter-discover-api/api/controllers/ipgeo.js
+++ unfetter-discover-api/api/controllers/ipgeo.js
@@ -7,7 +7,7 @@ const ipregex = require('ip-regex');
 class RequestError {
 
     static create() {
-        let re = new RequestError();
+        const re = new RequestError();
         re.err = {
             status: 500,
             title: '',
@@ -30,11 +30,7 @@ class RequestError {
     }
 
     addError(source = undefined, code = undefined, message = 'An unknown error has occurred.') {
-        this.err.errors.push({
-            code: code,
-            source: source,
-            message: message,
-        });
+        this.err.errors.push({ code, source, message, });
         console.log('added error', this);
         return this;
     }
@@ -81,17 +77,12 @@ class IPGeoProvider {
     }
 
     withHeaderKey(header, key) {
-        this.setKey = (req) => {
-            request.headers = {header: header, key: String(key)}
-        };
+        this.setKey = req => req.headers = { header, key, };
         return this;
     }
 
     withQueryParamKey(param, key) {
-        this.setKey = (req) => {
-            req.uri += (req.uri.indexOf('?') > 0) ? '&' : '?';
-            req.uri += String(param) + '=' + String(key);
-        };
+        this.setKey = req => req.uri = `${req.uri}${(req.uri.indexOf('?') > 0) ? '&' : '?'}${param}=${key}`;
         return this;
     }
 }
@@ -118,7 +109,7 @@ const IPGEO_PROVIDERS = [
 ];
 
 /**
- * @param {IPGeoProvider} provider 
+ * @param {IPGeoProvider} provider
  * @param {String[]} ip IP address to look up
  */
 function queryProviders(providers, ip, res, error = RequestError.create()) {
@@ -130,9 +121,9 @@ function queryProviders(providers, ip, res, error = RequestError.create()) {
     const request = {
         uri: provider.url.replace('*', ip),
         headers: {
-            'Accept': 'application/json',
+            Accept: 'application/json',
         },
-    }
+    };
     provider.setKey(request);
 
     const timeout = 1000 * 5; // millis
@@ -142,9 +133,7 @@ function queryProviders(providers, ip, res, error = RequestError.create()) {
         timeout
     }).then(response => response
         .json())
-        .then(json => {
-            return res.status(200).json({success: true, provider: provider.id, ...json});
-        })
+        .then(json => res.status(200).json({ success: true, provider: provider.id, ...json }))
         .catch(ex => {
             error.addError().source(provider.id).message(ex);
             queryProviders(providers, ip, res, error);
@@ -161,17 +150,20 @@ const lookup = (req, res) => {
     const ip = req.swagger.params.ip;
     if (!ip || !ip.value) {
         return res.status(400).json(RequestError.create().status(400).title('No IP Address Provided')
-            .message('You must provide an IP address to locate.').build());
+            .message('You must provide an IP address to locate.')
+            .build());
     }
     console.log('testing ip value', ip.value);
     try {
         if (!ipregex().test(ip.value)) {
             return res.status(400).json(RequestError.create().status(400).title('Invalid IP Address Provided')
-                .message(`The given value is not a valid IP address: ${ip.value}`).build());
+                .message(`The given value is not a valid IP address: ${ip.value}`)
+                .build());
         }
     } catch (ex) {
         return res.status(500).json(RequestError.create().status(500).title('Error performing IP check')
-            .message(`Could not perform IP validation: ${ex}`).build());
+            .message(`Could not perform IP validation: ${ex}`)
+            .build());
     }
 
     console.log('starting queries');
@@ -179,7 +171,8 @@ const lookup = (req, res) => {
     if (activeProviders.length === 0) {
         return res.status(503).json(RequestError.create().status(503).title('No IP-geo providers')
             .message('There are no active providers for geolocation services in our current configuration.  ' +
-                    'Please ask administrators to add new providers.').build())
+                    'Please ask administrators to add new providers.')
+            .build())
     }
 
     queryProviders(activeProviders, ip.value, res);
