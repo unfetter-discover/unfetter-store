d935a947e00ad8ad016b8c2a938290e2ccaa30a1
j987987 <j987897@users.noreply.github.com>
2018-04-25 12:52:16 -0400

    unit test

--- unfetter-discover-processor/src/adapters/string-enum-to-array.adapter.ts
+++ unfetter-discover-processor/src/adapters/string-enum-to-array.adapter.ts
@@ -1,3 +1,9 @@
+/**
+ * @param  {any} _enum
+ * @returns string
+ * @description Returns all possible values from enum
+ * WARNING: const enums may NOT be used with this function
+ */
 export default function stringEnumToArray(_enum: any): string[] {
     return Object.values(_enum);
 }
