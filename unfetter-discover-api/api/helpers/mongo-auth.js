e028d21696b3c1060bee84fef01106bb7d16bc8f
webiator <6414732+webiator@users.noreply.github.com>
2018-05-29 07:19:02 -0400

    Fixed lint errors

--- unfetter-discover-api/api/helpers/mongo-auth.js
+++ unfetter-discover-api/api/helpers/mongo-auth.js
@@ -1,34 +1,53 @@
-const mongoinit = require('../server/mongoinit.js')();
-const LocalStrategy = require('passport-local').Strategy;
-const scrypt = require("scrypt");
-const scryptParameters = scrypt.paramsSync(0.1, 1);
+// const mongoinit = require('../server/mongoinit.js')();
+// const UserAuth = require('../models/userauth.js');
+// const LocalStrategy = require('passport-local').Strategy;
+// const scrypt = require('scrypt');
+// const scryptParameters = scrypt.paramsSync(0.1, 1);
 
 /**
  * @todo This strategy is not fully implemented.
  */
 module.exports = {
 
-    build: function(config, env) {
-        console.log('building mongo strategy using', config, 'and', env);
-        const mongoStrategy = new LocalStrategy(function(username, password, callback) {
-            console.log('trying to authenticate', username, password);
-            UserAuth.findOne({userName: username}, function (err, user) {
-                if (err) {
-                    console.log('mongo error', err);
-                    return callback(err);
-                }
-                if (!user) {
-                    console.log('no such user');
-                    return callback(null, false, { message: 'Invalid username.' });
-                }
-                if (!scrypt.verifyHashSync(user.hash, password)) {
-                    console.log('password hash failed', user.hash);
-                    return callback(null, false, { message: 'Incorrect password.' });
-                }
-                return callback(null, user);
-            });
-        });
-        return mongoStrategy;
-    },
+    // build: (config, env) => {
+    //     const mongoStrategy = new LocalStrategy((username, password, callback) => {
+    //         UserAuth.findOne({ userName: username }, (err, user) => {
+    //             if (err) {
+    //                 console.log('mongo error', err);
+    //                 return callback(err);
+    //             }
+    //             if (!user) {
+    //                 console.log('no such user');
+    //                 return callback(null, false, { message: 'Invalid username.' });
+    //             }
+    //             if (!scrypt.verifyHashSync(user.hash, password)) {
+    //                 console.log('password hash failed', user.hash);
+    //                 return callback(null, false, { message: 'Incorrect password.' });
+    //             }
+    //             return callback(null, user);
+    //         });
+    //     });
+    //     return mongoStrategy;
+    // },
+
+    // options: { scope: ['user:email'] },
+
+    // search: user => ({ 'mongo.id': user.id }),
+
+    // sync: (storedUser, userinfo, approved) => {
+    //     storedUser.oauth = 'mongo';
+    //     if (!storedUser.github) {
+    //         storedUser.github = {
+    //             id: userinfo.id,
+    //             userName: null,
+    //             avatar: null,
+    //         };
+    //     }
+    //     storedUser.approved = approved;
+    //     storedUser.github.userName = userinfo.username;
+    //     if (userinfo._json.avatar_url) {
+    //         storedUser.github.avatar_url = userinfo._json.avatar_url;
+    //     }
+    // },
 
 };
