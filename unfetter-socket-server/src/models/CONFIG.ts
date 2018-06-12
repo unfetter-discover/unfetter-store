6217f1130491b162c9166a6edf99e9a32d4dc634
j987987 <30236110+j987987@users.noreply.github.com>
2018-06-12 09:33:04 -0400

    Issue 1139 (#203)
    * private subdir for configs
    
    * added handling for legacy config file location
    
    * obs data filter (#202)
    
    * private subdir for configs
    
    * added handling for legacy config file location

--- unfetter-socket-server/src/models/CONFIG.ts
+++ unfetter-socket-server/src/models/CONFIG.ts
@@ -3,4 +3,20 @@ import * as path from 'path';
 
 import { ConfigModel } from './configModel';
 
-export const CONFIG: ConfigModel = JSON.parse(fs.readFileSync(path.join(__dirname, '../../config/private-config.json'), 'utf8'));
+let tempConfig: ConfigModel = null;
+
+// TODO remove handling for legacy path
+const currentPath = path.join(__dirname, '..', '..', 'config', 'private', 'private-config.json');
+const legacyPath = path.join(__dirname, '..', '..', 'config', 'private-config.json');
+
+if (fs.existsSync(currentPath)) {
+    tempConfig = JSON.parse(fs.readFileSync(currentPath, 'utf8'));
+} else if (fs.existsSync(legacyPath)) {
+    tempConfig = JSON.parse(fs.readFileSync(legacyPath, 'utf8'));
+    console.log('WARNING: You are using a legacy version of the of the configuration file, please rerun the unfetter-store/unfetter-utils/api_configuration_tool.py script');
+} else {
+    console.error('Please use unfetter-store/unfetter-utils/api_configuration_tool.py to create private-config.json');
+    process.exit(1);
+}
+
+export const CONFIG = tempConfig;
