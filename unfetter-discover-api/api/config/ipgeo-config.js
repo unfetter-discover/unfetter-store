c61410a48814ecef643ef0ef0170c8bf8a2ea829
webiator <canderson1@keywcorp.com>
2018-08-03 09:10:32 -0400

    resets order of IP services, were changed for failure testing (#238)

--- unfetter-discover-api/api/config/ipgeo-config.js
+++ unfetter-discover-api/api/config/ipgeo-config.js
@@ -5,16 +5,16 @@ const IPGeoProvider = require('../models/ipgeo').IPGeoProvider;
  * the nature that the providers can provide behavior (functions), and that cannot be overridden in a JSON file.
  */
 const IPGEO_PROVIDERS = [
-    // another free service allows up to 1500 requests per day, can be bulk(?)
-    new IPGeoProvider('ipdata.co', 'https://api.ipdata.co/*')
-        .withTranslate(json => ({ ...json, country: json.country_code })),
-
     // free-version service allows up to 1000 requests per day, no bulk queries
     new IPGeoProvider('ipapi.co', 'https://ipapi.co/*/json/'),
 
     // new endpoint for freegeoip with personal key ()
     new IPGeoProvider('ipstack.com', 'http://api.ipstack.com/*', true, ',')
         .withQueryParamKey('access_key', '64a9512c200c1edd5b5b521a441f0eff')
+        .withTranslate(json => ({ ...json, country: json.country_code })),
+
+    // another free service allows up to 1500 requests per day, can be bulk(?)
+    new IPGeoProvider('ipdata.co', 'https://api.ipdata.co/*')
         .withTranslate(json => ({ ...json, country: json.country_code })),
 ];
 
