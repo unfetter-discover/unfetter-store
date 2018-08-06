3a841388de6f8009ae9b3f69e461835297cfa713
j987987 <30236110+j987987@users.noreply.github.com>
2018-08-06 13:19:36 -0400

    Issue 1336 (#239)
    * removed gzip
    
    * updated socket server references
    
    * updated user helper

--- unfetter-discover-api/api/helpers/user.js
+++ unfetter-discover-api/api/helpers/user.js
@@ -1,13 +1,21 @@
 const getAvatarUrl = user => {
+    if (user && user.auth && user.auth.avatar_url) {
+        return user.auth.avatar_url;
+    }
+
+    // @deprecated
+    // Support for legacy user model, can be deleted in the future
     if (user && user.oauth) {
         const oauth = user.oauth;
         if (user[oauth] && user[oauth].avatar_url) {
             return user[oauth].avatar_url;
         }
     }
+
+    // @deprecated
     // Support for legacy user model, can be deleted in the future
-    if (user.guthub && user.guthub.avatar_url) {
-        return user.guthub.avatar_url;
+    if (user.guthub && user.github.avatar_url) {
+        return user.github.avatar_url;
     }
     return null;
 };
