a9c4c762809cc2a4db59fad6dc547b90643264bb
j987987 <j987897@users.noreply.github.com>
2018-08-30 10:33:43 -0400

    added unit tests related to issue 1395

---
+++ unfetter-discover-api/test/api/config/passport-config.spec.js
@@ -0,0 +1,181 @@
+const should = require('should');
+
+const passportConfig = require('../../../api/config/passport-config');
+
+describe('passportConfig', () => {
+    let mockReq;
+    let mockRes;
+    
+    beforeEach(() => {
+        mockReq = {
+            user: null
+        };
+
+        mockRes = {
+            status() {
+                return this;
+            },
+            json(json) {
+                return json;
+            }
+        };
+    });
+
+    describe('jwtStandard', () => {
+        it('should response with an error if no user', () => {
+            const retVal = passportConfig.jwtStandard(mockReq, mockRes, () => {});
+            should.exist(retVal);
+        });
+
+        it('should response with an error if user is not approved', () => {
+            mockReq.user = {
+                approved: false,
+                locked: false
+            };
+            const retVal = passportConfig.jwtStandard(mockReq, mockRes, () => {});
+            should.exist(retVal);
+        });
+
+        it('should response with an error if user is locked', () => {
+            mockReq.user = {
+                approved: true,
+                locked: true
+            };
+            const retVal = passportConfig.jwtStandard(mockReq, mockRes, () => {});
+            should.exist(retVal);
+        });
+
+        it('should response invoke callback with approved unlocked user', done => {
+            mockReq.user = {
+                approved: true,
+                locked: false
+            };
+            let retVal;
+            const callback = () => {
+                should.equal(true, true);
+                should.not.exist(retVal);
+                done();
+            };
+            retVal = passportConfig.jwtStandard(mockReq, mockRes, callback);
+        });
+    });
+
+    describe('jwtAdmin', () => {
+        it('should response with an error if no user', () => {
+            const retVal = passportConfig.jwtAdmin(mockReq, mockRes, () => {});
+            should.exist(retVal);
+        });
+
+        it('should response with an error if user is not approved', () => {
+            mockReq.user = {
+                approved: false,
+                locked: false,
+                role: 'ADMIN'
+            };
+            const retVal = passportConfig.jwtStandard(mockReq, mockRes, () => {});
+            should.exist(retVal);
+        });
+
+        it('should response with an error if user is locked', () => {
+            mockReq.user = {
+                approved: true,
+                locked: true,
+                role: 'ADMIN'
+            };
+            const retVal = passportConfig.jwtAdmin(mockReq, mockRes, () => {});
+            should.exist(retVal);
+        });
+
+        it('should response with an error if user is not an admin', () => {
+            mockReq.user = {
+                approved: true,
+                locked: true,
+                role: 'NOT_ADMIN'
+            };
+            const retVal = passportConfig.jwtAdmin(mockReq, mockRes, () => {});
+            should.exist(retVal);
+        });
+
+        it('should response invoke callback with approved unlocked admin user', done => {
+            mockReq.user = {
+                approved: true,
+                locked: false,
+                role: 'ADMIN'
+            };
+            let retVal;
+            const callback = () => {
+                should.equal(true, true);
+                should.not.exist(retVal);
+                done();
+            };
+            retVal = passportConfig.jwtAdmin(mockReq, mockRes, callback);
+        });
+    });
+    
+    describe('jwtOrganizations', () => {
+        it('should response with an error if no user', () => {
+            const retVal = passportConfig.jwtOrganizations(mockReq, mockRes, () => {});
+            should.exist(retVal);
+        });
+
+        it('should response with an error if user is not approved', () => {
+            mockReq.user = {
+                approved: false,
+                locked: false,
+                role: 'ADMIN'
+            };
+            const retVal = passportConfig.jwtOrganizations(mockReq, mockRes, () => {});
+            should.exist(retVal);
+        });
+
+        it('should response with an error if user is locked', () => {
+            mockReq.user = {
+                approved: true,
+                locked: true,
+                role: 'ADMIN'
+            };
+            const retVal = passportConfig.jwtOrganizations(mockReq, mockRes, () => {});
+            should.exist(retVal);
+        });
+
+        it('should response with an error if user is a standard user', () => {
+            mockReq.user = {
+                approved: true,
+                locked: true,
+                role: 'STANDARD_USER'
+            };
+            const retVal = passportConfig.jwtOrganizations(mockReq, mockRes, () => {});
+            should.exist(retVal);
+        });
+
+        it('should response invoke callback with approved unlocked admin user', done => {
+            mockReq.user = {
+                approved: true,
+                locked: false,
+                role: 'ADMIN'
+            };
+            let retVal;
+            const callback = () => {
+                should.equal(true, true);
+                should.not.exist(retVal);
+                done();
+            };
+            retVal = passportConfig.jwtOrganizations(mockReq, mockRes, callback);
+        });
+
+        it('should response invoke callback with approved unlocked org leader user', done => {
+            mockReq.user = {
+                approved: true,
+                locked: false,
+                role: 'ORG_LEADER'
+            };
+            let retVal;
+            const callback = () => {
+                should.equal(true, true);
+                should.not.exist(retVal);
+                done();
+            };
+            retVal = passportConfig.jwtOrganizations(mockReq, mockRes, callback);
+        });
+    });
+});
