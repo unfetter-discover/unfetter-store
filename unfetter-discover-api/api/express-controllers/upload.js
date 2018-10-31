16dd4c9846f07cd6971aa911762481b98d0c01c8
j987987 <j987897@users.noreply.github.com>
2018-10-31 10:49:28 -0400

    url pdf to text

--- unfetter-discover-api/api/express-controllers/upload.js
+++ unfetter-discover-api/api/express-controllers/upload.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const fetch = require('node-fetch');
+const getFileType = require('file-type');
+const pdfParse = require('pdf-parse');
 
 const multerHelpers = require('../helpers/multer-helper');
 const config = require('../config/config');
+const fetchHelpers = require('../helpers/fetch_helper');
 
 const router = express.Router();
 
@@ -28,6 +32,75 @@ router.post('/files', (req, res, next) => {
         };
         return res.status(201).json(response);
     });
+});
+
+// For text extraction only
+const ALLOW_FILE_TYPES = [
+    'pdf',
+    'application/pdf'
+];
+
+router.post('/extract-text/:fileType', async (req, res) => {
+    const { fileType } = req.params;
+    const { url } = req.body.data.attributes;
+
+    if (!fileType || !url) {
+        return res.status(400).json({
+            errors: [{
+                status: 400, source: '', title: 'Error', code: '', detail: 'Filetype and URL is required'
+            }]
+        });
+    }
+
+    if (!ALLOW_FILE_TYPES.includes(fileType)) {
+        return res.status(400).json({
+            errors: [{
+                status: 400, source: '', title: 'Error', code: '', detail: 'Filetype is not supported'
+            }]
+        });
+    }
+
+    try {
+        const fetchResults = await fetch(url, fetchHelpers.instanceOptions);
+        const buffer = await fetchHelpers.getResultBuffer(fetchResults);
+        const bufferFileType = getFileType(buffer);
+        if (bufferFileType.ext !== fileType && bufferFileType.mime !== fileType) {
+            return res.status(500).json({
+                errors: [{
+                    status: 500, source: '', title: 'Error', code: '', detail: 'The provided URL did not contain the correct file type.'
+                }]
+            });
+        }
+        let extractedText;
+
+        switch (fileType) {
+        case 'pdf':
+        case 'application/pdf':
+            const { text } = await pdfParse(buffer);
+            extractedText = text;
+            break;
+        default:
+            // This should never be reached
+            throw new Error('Unreachable switch statement reached');
+        }
+
+        return res.json({
+            data: {
+                attributes: {
+                    url,
+                    ext: bufferFileType.ext,
+                    contentType: bufferFileType.mime,
+                    extractedText
+                }
+            }
+        });
+    } catch (error) {
+        return res.status(500).json({
+            errors: [{
+                status: 500, source: '', title: 'Error', code: '', detail: 'An unknown error has occurred.'
+            }]
+        });
+    }
 });
 
 // TODO
